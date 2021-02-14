52ffd7955e599e99f71069cb75e4841a9286c71a
Patricio Gonzalez <patote.gonzalez@gmail.com>
2021-02-14 06:11:32 -0300

    current user and signout

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,8 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('hi there!');
+  // REF !req.session || !req.session.jwt = !req.session?.jwt
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!,
+    );
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };

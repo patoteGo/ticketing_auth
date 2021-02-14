ef5956afafd590826ae9280238aad1bdca344647
Patricio Gonzalez <patote.gonzalez@gmail.com>
2021-02-14 04:56:46 -0300

    add jwt as secret

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
@@ -30,6 +32,17 @@ router.post('/api/users/signup', [
 
   const user = User.build({ email, password });
   await user.save();
+
+  const userJwt = jwt.sign({
+    id: user.id,
+    email: user.email,
+  }, process.env.JWT_KEY!);
+
+  // Store it on session object
+  // req.session.jwt = userJwt; no funciona
+  req.session = {
+    jwt: userJwt,
+  };
 
   res.status(201).send(user);
 });
